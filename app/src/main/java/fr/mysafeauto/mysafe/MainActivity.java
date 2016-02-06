2a848503d64f5d97b82286bbd272126a662520b0
rahghul95 <rahghul95@hotmail.fr>
2016-02-06 14:46:10 +0100

    Second commit

--- app/src/main/java/fr/mysafeauto/mysafe/MainActivity.java
+++ app/src/main/java/fr/mysafeauto/mysafe/MainActivity.java
@@ -85,7 +85,7 @@ public class MainActivity extends AppCompatActivity
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Snackbar.make(view, "Code to implement to show the details about the app !", Snackbar.LENGTH_LONG)
+                Snackbar.make(view, "Code to implement to show the details about the app", Snackbar.LENGTH_LONG)
                         .setAction("Action", null).show();
             }
         });
